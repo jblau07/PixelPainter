d0877351a57dac02ef9469180fc81d6bf425a13c
Zubin Dodhi <zubindodhi@Zubins-MacBook-Pro.local>
2018-01-04 13:38:27 -1000

    swatch

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,83 +1,51 @@
- let ppCanvas = document.getElementById("pixelPainter");
+let ppCanvas = document.getElementById("pixelPainter");
 
- let gridBox = document.createElement("div")
- gridBox.className = "gridBox";
- ppCanvas.appendChild(gridBox);
+let gridBox = document.createElement("div")
+//  swatchBox.style.backgroundColor = "blue";
+gridBox.className = "gridBox";
+ppCanvas.appendChild(gridBox);
 
- let swatchBox = document.createElement("div");
- swatchBox.className = "swatchBox";
- ppCanvas.appendChild(swatchBox);
- // let gird = document.createElement("div").id = "ppGrid";
- // let erase = document.createElement("div").id = "ppErase";
- // let clear = document.createElement("div").id = "ppClear";
- // let legend = document.createElement("div").id = "ppLegend";
+let swatchBox = document.createElement("div");
+swatchBox.style.backgroundColor = "blue";
+swatchBox.className = "swatchBox";
+ppCanvas.appendChild(swatchBox);
 
-
- // let pixel = function(){
- // for (x = 0; x < this.getHeight(); x++) {
- //     for (let y = 0; y < this.getWidth(); y++) {
- //         obj = this.getPixel(x,y);
-
- //     }
-
- // }
- // }
- // pixel.appendChild("ppGrid");
-
- let createGrid = function (x, y, element) {
-     for (let i = 1; i <= x; i++) {
-         let row = document.createElement("div");
-         row.className = "row";
-        
-
-         for (let k = 1; k <= y; k++) {
-             let cell = document.createElement("div");
-             cell.className = "cell";
-             row.appendChild(cell);
-         }
-         element.appendChild(row);
-     }
- }
-
-//  let colors = ["red", "blue", "yellow", "orange", "purple", "green", "gray", "brown", "black", "white"];
-//  let colorButtons = document.getElementsByClassName("cell");
-
-
- 
-//  let button = document.createElement("button");
-//  button.innerHTML = "p";
-
-//  let purpleB = document.getElementsByTagName("purpleB");
-//  purpleB.appendChild(button);
-
- 
- 
-/*let createGrid2 = function (x, y) {
+ function createGrid (x, y, element) {
     for (let i = 1; i <= x; i++) {
-        let row2 = document.createElement("div");
-        row2.className = "row2";
-       
+        let row = document.createElement("div");
+        row.className = "row";
 
         for (let k = 1; k <= y; k++) {
-            let cell2 = document.createElement("div");
-            cell2.className = "cell2";
-            row2.appendChild(cell2);
+            let cell = document.createElement("div");
+            cell.className = "cell";
+            row.appendChild(cell);
+        
+            }
+            element.appendChild(row);
         }
-        ppCanvas2.appendChild(row2);
-    }
-}
- */
- 
- createGrid(3, 6, gridBox);
+    };
 
- let grid2 = createGrid(30, 30, swatchBox);
-//  grid1.id = "legend";
+    createGrid(2, 2, swatchBox);
 
- //let grid2 = createGrid(30, 30);
-// grid2.id = "blankGrid";
+    let grid2 = createGrid(30, 30, gridBox);
 
-//  let grid2 = createGrid(3, 3);
-//  grid2.id = ("legend");
- 
+    let eraseButton = document.createElement("button");
+    eraseButton.innerHTML = "Erase";
 
- 
+    //  let body = document.getElementsByClassName("swatchBox");
+    ppCanvas.appendChild(eraseButton);
+
+    eraseButton.addEventListener("click", function () {
+        alert("heyyy");
+    });
+
+
+    let clearButton = document.createElement("button");
+    clearButton.innerHTML = "Clear";
+
+    let body2 = document.getElementById("pixelPainter");
+    body2.appendChild(clearButton);
+
+    clearButton.addEventListener("click", function () {
+        alert("heyyy");
+    });
