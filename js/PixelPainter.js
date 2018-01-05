3aabac299eda34b50ce96dc344cde0a046f7c928
jblau07 <jlau7248@gmail.com>
2018-01-05 10:45:46 -1000

    fixed indentation

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -14,66 +14,66 @@ ppCanvas.appendChild(swatchBox);
 
 // create swatch grid
 function createSwatchGrid(x, y, element, ) {
-    let counter = 0;
+	let counter = 0;
 
-    for (let i = 1; i <= x; i++) {
-        let row = document.createElement("div");
-        row.className = "row";
+	for (let i = 1; i <= x; i++) {
+		let row = document.createElement("div");
+		row.className = "row";
 
-        for (let j = 1; j <= y; j++) {
-            let cell = document.createElement("div");
-            cell.className = "cell";
-            cell.style.backgroundColor = colors[counter++];
-            row.appendChild(cell);
+		for (let j = 1; j <= y; j++) {
+			let cell = document.createElement("div");
+			cell.className = "cell";
+			cell.style.backgroundColor = colors[counter++];
+			row.appendChild(cell);
 
-        }
-        element.appendChild(row);
-    }
+		}
+		element.appendChild(row);
+	}
 };
 
 // function to interact with swatch
 swatchBox.addEventListener("click", function () {
-    colorMemory = event.target.style.backgroundColor;
-    console.log("swatch")
+	colorMemory = event.target.style.backgroundColor;
+	console.log("swatch")
 });
 gridBox.addEventListener("mousedown", function () {
-    isDown = true; 
-    console.log("mousedown")
+	isDown = true;
+	console.log("mousedown")
 });
 gridBox.addEventListener("mouseup", function () {
-    isDown = false; 
-    console.log("mouseup")
+	isDown = false;
+	console.log("mouseup")
 });
 
-gridBox.addEventListener("click", function (){
-    if (event.target.classList.contains("cell2")) 
-        event.target.style.backgroundColor = colorMemory;
-    });
+gridBox.addEventListener("click", function () {
+	if (event.target.classList.contains("cell2"))
+		event.target.style.backgroundColor = colorMemory;
+});
 
 gridBox.addEventListener("mouseover", function () {
-    if (isDown === true && event.target.classList.contains("cell2")) {
-        event.target.style.backgroundColor = colorMemory;
-        console.log("paint")
-    }
+	if (isDown === true && event.target.classList.contains("cell2")) {
+		event.target.style.backgroundColor = colorMemory;
+		console.log("paint")
+	}
 });
 
 // create blank grid
 function createBlankGrid(x, y, element) {
-    let counter = 0;
+	let counter = 0;
 
-    for (let i = 1; i <= x; i++) {
-        let row2 = document.createElement("div");
-        row2.className = "row2";
+	for (let i = 1; i <= x; i++) {
+		let row2 = document.createElement("div");
+		row2.className = "row2";
 
-        for (let j = 1; j <= y; j++) {
-            let cell2 = document.createElement("div");
-            cell2.className = "cell2";
-            cell2.style.backgroundColor = "white";
-            row2.appendChild(cell2);
+		for (let j = 1; j <= y; j++) {
+			let cell2 = document.createElement("div");
+			cell2.className = "cell2";
+			cell2.style.backgroundColor = "white";
+			row2.appendChild(cell2);
 
-        }
-        element.appendChild(row2);
-    }
+		}
+		element.appendChild(row2);
+	}
 };
 
 createSwatchGrid(2, 10, swatchBox);
@@ -86,10 +86,9 @@ eraseButton.innerHTML = "Erase";
 ppCanvas.appendChild(eraseButton);
 
 eraseButton.addEventListener("click", function () {
-    console.log("erase something")
-    colorMemory = "white";
+	console.log("erase something")
+	colorMemory = "white";
 });
-
 
 let clearButton = document.createElement("button");
 clearButton.id = "clearButton"
@@ -97,16 +96,16 @@ clearButton.innerHTML = "Clear";
 ppCanvas.appendChild(clearButton);
 
 clearButton.addEventListener("click", function () {
-    console.log('hello');
-    let myGrid = gridBox.querySelectorAll('.row2 .cell2');
-    for (let index = 0; index < myGrid.length; index++) {
-        myGrid[index].style.backgroundColor = 'white';
-    }
+	console.log('all clear');
+	let myGrid = gridBox.querySelectorAll('.row2 .cell2');
+	for (let index = 0; index < myGrid.length; index++) {
+		myGrid[index].style.backgroundColor = 'white';
+	}
 });
 // easterEggs
 eraseButton.addEventListener("dblclick", function () {
-    window.location.href = "https://github.com/jblau07";
+	window.location.href = "https://github.com/jblau07";
 });
 clearButton.addEventListener("dblclick", function () {
-    window.location.href = "https://github.com/DODHI5";
+	window.location.href = "https://github.com/DODHI5";
 });
