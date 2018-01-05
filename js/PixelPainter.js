1d16e9ed374401532cf005fb6c9dafdeaa6e5517
jblau07 <jlau7248@gmail.com>
2018-01-04 15:07:37 -1000

    completed color/grid array function

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,51 +1,70 @@
-let ppCanvas = document.getElementById("pixelPainter");
+const ppCanvas = document.getElementById("pixelPainter");
 
-let gridBox = document.createElement("div")
-//  swatchBox.style.backgroundColor = "blue";
+let gridBox = document.createElement("div");
 gridBox.className = "gridBox";
 ppCanvas.appendChild(gridBox);
 
+let colors = ['blue', 'red', 'green', 'yellow'];
+let colors2 = "white";
+
 let swatchBox = document.createElement("div");
-swatchBox.style.backgroundColor = "blue";
+//swatchBox.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
 swatchBox.className = "swatchBox";
 ppCanvas.appendChild(swatchBox);
 
- function createGrid (x, y, element) {
+
+
+function createGrid(x, y, element, bgColor) {
+    let counter = 0;
+    
     for (let i = 1; i <= x; i++) {
         let row = document.createElement("div");
         row.className = "row";
 
-        for (let k = 1; k <= y; k++) {
+        for (let j = 1; j <= y; j++) {
             let cell = document.createElement("div");
             cell.className = "cell";
+            cell.style.backgroundColor = bgColor[counter ++];
             row.appendChild(cell);
-        
-            }
-            element.appendChild(row);
+
         }
-    };
+        element.appendChild(row);
+    }
+};
 
-    createGrid(2, 2, swatchBox);
+createGrid(2, 2, swatchBox, colors);
 
-    let grid2 = createGrid(30, 30, gridBox);
-
-    let eraseButton = document.createElement("button");
-    eraseButton.innerHTML = "Erase";
-
-    //  let body = document.getElementsByClassName("swatchBox");
-    ppCanvas.appendChild(eraseButton);
-
-    eraseButton.addEventListener("click", function () {
-        alert("heyyy");
-    });
+createGrid(30, 30, gridBox, colors2);
 
 
-    let clearButton = document.createElement("button");
-    clearButton.innerHTML = "Clear";
 
-    let body2 = document.getElementById("pixelPainter");
-    body2.appendChild(clearButton);
+//function fillSwatchBox (container){}
 
-    clearButton.addEventListener("click", function () {
-        alert("heyyy");
-    });
+let indivColor = swatchBox.getElementsByClassName("cell");
+
+// function createPalette () {
+//     indivColor.forEach(function(cell, index){
+//         swatchBox.style.backgroundColor = colors[index];
+//     })
+//  }
+
+
+
+
+
+let eraseButton = document.createElement("button");
+eraseButton.innerHTML = "Erase";
+ppCanvas.appendChild(eraseButton);
+
+eraseButton.addEventListener("click", function () {
+    alert("heyyy");
+});
+
+
+let clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+ppCanvas.appendChild(clearButton);
+
+clearButton.addEventListener("click", function () {
+    alert("heyyy");
+});
